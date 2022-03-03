5a1b4bf97a2f7d3e8384fc54e39387597837440c
Alisa Raevskaya <alisa@madebyforward.com>
2022-03-03 23:31:15 +0300

    Add url

--- src/views/Leagues.vue
+++ src/views/Leagues.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="container">
     <h1>Лиги</h1>
-    <p>{{key}}</p>
+
     <app-search />
     <div class="league-cards">
-      <div class="card">
+      <div class="card" v-for="item in leagues" :key="item.id">
+        <div class="card-content">
+          <p class="card-title">League {{item.name}}</p>
+          <p class="card-subtitle">Country{{item.area.name}}</p>
+        </div>
+      </div>
+      <!-- <div class="card">
         <div class="card-content">
           <p class="card-title">League</p>
           <p class="card-subtitle">Country</p>
@@ -51,13 +57,7 @@
           <p class="card-title">League</p>
           <p class="card-subtitle">Country</p>
         </div>
-      </div>
-      <div class="card">
-        <div class="card-content">
-          <p class="card-title">League</p>
-          <p class="card-subtitle">Country</p>
-        </div>
-      </div>
+      </div> -->
     </div>
     <app-pagination />
   </div>
@@ -68,6 +68,7 @@ const baseUrl = "https://api.football-data.org/v2/competitions";
 const apiKey = process.env.VUE_APP_API_KEY;
 import AppSearch from "../components/Search.vue";
 import AppPagination from "../components/Pagination.vue";
+import axios from "axios";
 
 //Вызов при переходе на страницу лиги - GET http://api.football-data.org/v2/competitions/{id}/matches
 //где {id} - competitions[].id
@@ -78,12 +79,26 @@ export default {
     AppPagination,
   },
   data() {
-    return {};
+    return {
+      leagues: [],
+    };
   },
   computed: {
     key(){
-     console.log(process.env.VUE_APP_API_KEY);
+     return apiKey;
     }
+  },
+ mounted() {
+    axios({
+      method: "get",
+      url: "https://api.football-data.org/v2/competitions",
+      headers: { "X-Auth-Token": '1e76ed510bd246519dedbf03833e5322' },
+    })
+      .then((response)=>{this.leagues = response.data.competitions}
+      );
+      // .catch(function (error) {
+      //   console.log("error");
+      // });
   },
 };
 </script>
